3736d29ea9971f3d713d684974b3a7886a82baf0
abidkyo <kyotasuna95@gmail.com>
2023-06-12 01:06:48 +0200

    ENH remote data

---
+++ lib/models/remote_data.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class RemoteData{
+  final String code;
+  final Color color;
+  final Widget? widget;
+
+  RemoteData({required this.code, required this.color, this.widget});
+}
